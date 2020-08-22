ad672cf2b118250d49a5e4b1cb120f882addd961
Itay Wolfish <wo.itay@gmail.com>
2020-08-22 17:38:30 +0300

    init

---
+++ dist/app/header/header.component.d.ts
@@ -0,0 +1,6 @@
+import { Component } from '../../services/component';
+import "./header.style.less";
+export declare class HeaderComponent extends Component {
+    constructor(ref: Element);
+}
+//# sourceMappingURL=header.component.d.ts.map
