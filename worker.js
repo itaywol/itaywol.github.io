d9fb6bd031b9c2387f13a702f6661380dbd70f4f
Itay Wolfish <itay@linnovate.net>
2020-11-20 12:38:32 +0200

    added resource to offline, improved manifest

--- worker.js
+++ worker.js
@@ -15,15 +15,23 @@ Copyright 2015, 2019, 2020 Google LLC. All Rights Reserved.
 // previously cached resources to be updated from the network.
 const OFFLINE_VERSION = 1;
 const CACHE_NAME = "offline";
-const OFFLINE_URLS = ["/", "/scripts/main.js", "/styles/style.css"];
+const CACHED_URLS = [
+  "/",
+  "/favicon.ico",
+  "/styles/style.css",
+  "/scripts/main.js",
+  "https://unpkg.com/pwacompat",
+];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
     (async () => {
       const cache = await caches.open(CACHE_NAME);
-      await Promise.all(OFFLINE_URLS.map(OFFLINE_URL => {
-        cache.add(new Request(OFFLINE_URL, { cache: "reload" }))
-      }))
+      await Promise.all(
+        CACHED_URLS.map((CACHE_URL) => {
+          return cache.add(new Request(CACHE_URL, { cache: "reload" }));
+        })
+      );
     })()
   );
   self.skipWaiting();
@@ -54,13 +62,8 @@ self.addEventListener("fetch", (event) => {
           return networkResponse;
         } catch (error) {
           console.log("Fetch failed; returning offline page instead.", error);
-
           const cache = await caches.open(CACHE_NAME);
-          let cachedResponse
-          await Promise.all(OFFLINE_URLS.map(OFFLINE_URL => {
-            cachedResponse = cache.match(OFFLINE_URL);
-            return cachedResponse;
-          }));
+          let cachedResponse = await cache.match(event.request.url);
           return cachedResponse;
         }
       })()
